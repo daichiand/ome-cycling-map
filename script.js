682ee9e6916dc699874ffb3f6c627be5a8f86aae
daichiand <daichiand4649@gmail.com>
2026-08-07 20:16:09 +0900

    add sightseeing data

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-// 青梅法人会エリア サイクリング・観光マップ
+// 青梅法人会エリア
+// サイクリング・観光マップ
 
 // 地図作成
 const map = L.map('map').setView(
@@ -30,23 +31,16 @@ if (navigator.geolocation) {
 
             L.marker([lat,lng])
             .addTo(map)
-            .bindPopup(
-                "現在地"
-            )
+            .bindPopup("現在地")
             .openPopup();
 
-
-            map.setView(
-                [lat,lng],
-                14
-            );
 
         },
 
         function(){
 
             console.log(
-                "現在地を取得できませんでした"
+                "現在地取得失敗"
             );
 
         }
@@ -56,47 +50,67 @@ if (navigator.geolocation) {
 }
 
 
-// 仮の観光スポットデータ
 
-const spots = [
+// JSONから観光データ読み込み
 
-{
-    name:"御岳渓谷",
-    lat:35.8027,
-    lng:139.1804,
-    text:"多摩川沿いの美しい渓谷。サイクリングにもおすすめです。"
-},
+fetch("data/sightseeing.json")
 
-{
-    name:"釜の淵公園",
-    lat:35.7904,
-    lng:139.2548,
-    text:"川沿いで休憩できる青梅の自然スポットです。"
-},
+.then(function(response){
 
-{
-    name:"青梅駅",
-    lat:35.7876,
-    lng:139.2756,
-    text:"青梅観光のスタート地点です。"
-}
+    return response.json();
 
-];
+})
 
 
-// マーカー表示
+.then(function(spots){
 
-spots.forEach(function(spot){
 
-    L.marker(
-        [spot.lat,spot.lng]
-    )
-    .addTo(map)
-    .bindPopup(
+    spots.forEach(function(spot){
+
+
+        L.marker(
+            [
+                spot.lat,
+                spot.lng
+            ]
+        )
+
+        .addTo(map)
+
+        .bindPopup(
+
         `
         <h3>${spot.name}</h3>
-        <p>${spot.text}</p>
+
+        <p>
+        <b>カテゴリー：</b>
+        ${spot.category}
+        </p>
+
+        <p>
+        ${spot.description}
+        </p>
+
+        <p>
+        🚴 ${spot.cycling}
+        </p>
+
         `
+
+        );
+
+
+    });
+
+
+})
+
+
+.catch(function(error){
+
+    console.log(
+        "データ読み込みエラー",
+        error
     );
 
 });
