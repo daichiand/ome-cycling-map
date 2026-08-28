8e320bcf5e932128ef218ad98d61c2d163cfd2ff
r1kubooolm <mikankinako04@gmail.com>
2026-08-28 14:20:01 +0900

    feat: connect public tourism data API

--- 20260828_map-config.js
+++ 20260828_map-config.js
@@ -1,1 +1,1 @@
-window.OME_MAP_CONFIG = { sheetApiUrl: "", sampleDataUrl: "data/sightseeing.json" };
+window.OME_MAP_CONFIG = { sheetApiUrl: "https://script.google.com/macros/s/AKfycbyZWpFQxMvnEglDS7FWYvzpUTwwRA9o40m9hpctt1o_9Wep7AK5GowvIahg6ab7jfj5/exec", sampleDataUrl: "data/sightseeing.json" };
